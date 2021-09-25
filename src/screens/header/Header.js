2fce8490898b201a1708b307ab5cab612c9d4337
janayAl <jsallen298705@gmail.com>
2021-09-25 17:46:49 -0500

    installed typed and added strings of an array of who i am to auto display

---
+++ src/screens/header/Header.js
@@ -0,0 +1,26 @@
+//snippet short cut rfce
+import React from 'react'
+import Typed from 'react-typed'
+
+function Header() {
+    return (
+        <div>
+            <h1>Who am I?</h1>
+            {/* passes an array of whatever you want to type */}
+            <Typed
+                strings={[
+                    'An Educator',
+                    'A Developer',
+                    'A Creative',
+                    'A Student',
+                    'An analyzer']}
+                typeSpeed={40}
+                backSpeed={50}
+
+                loop
+            />
+        </div>
+    )
+}
+
+export default Header
