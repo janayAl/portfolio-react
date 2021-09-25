195a5682de3c32ee50ab03be2d67b25d774c1819
janayAl <jsallen298705@gmail.com>
2021-09-25 16:50:09 -0500

    added particile animation install and import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navigation from './screens/navigationBar/NavigationBar'
+import ParticleAnimation from 'react-particle-animation'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <ParticleAnimation />
     </div>
   );
 }
