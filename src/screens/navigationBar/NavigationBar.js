f5e56330d50fdd083ba94c338f3455ff54fe21dd
janayAl <jsallen298705@gmail.com>
2021-09-25 14:51:07 -0500

    adding functions to navigation bar for the links

--- src/screens/navigationBar/NavigationBar.js
+++ src/screens/navigationBar/NavigationBar.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import { navigationLinks } from '../../helpers/navigationLinks'
+
+//create a function for your nav links. pass through an element and index
+//e.name is from the objects created in the nav links file
+//render this inside the collapse
+function createLinks() {
+    return navigationLinks.map((e, idx) => ( //idx stands for index
+        <Nav.Link key={idx} href={e.ref}>{e.name}</Nav.Link> //ref because this is what we called it in the navlinks file
+    ))
+}
 
 function NavigationBar() {
     return (
         <div id="home">
             <Navbar>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">JANAY A.</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse>
+
+                </Navbar.Collapse>
             </Navbar>
         </div>
     )
