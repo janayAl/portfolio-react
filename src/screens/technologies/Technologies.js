6fb351590e325c3b40b337983f4e337d8efa73a6
janayAl <jsallen298705@gmail.com>
2021-09-27 22:56:37 -0500

    commented out all of the technologies folders and files

--- src/screens/technologies/Technologies.js
+++ src/screens/technologies/Technologies.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import DevIcon from "devicon-react-svg";
-import { techs } from '../../helpers/techs'
+// import React from 'react'
+// import DevIcon from "devicon-react-svg";
+// import { techs } from '../../helpers/techs'
 
-// use cheatsheet from devicon docs
-function Technologies() {
-    return (
-        <div class="tech_icons">
-            <DevIcon icon='react' />
-            <DevIcon icon='javascript' />
-            <DevIcon icon='css3' />
-            <DevIcon icon='html5' />
-            <DevIcon icon='mysql' />
-            <DevIcon icon='mongodb' />
-            <DevIcon icon='bootstrap' />
-            <DevIcon icon='github' />
-            <DevIcon icon='nodejs' />
+// // use cheatsheet from devicon docs
+// function Technologies() {
+//     return (
+//         <div class="tech_icons">
+//             <DevIcon icon='react' />
+//             <DevIcon icon='javascript' />
+//             <DevIcon icon='css3' />
+//             <DevIcon icon='html5' />
+//             <DevIcon icon='mysql' />
+//             <DevIcon icon='mongodb' />
+//             <DevIcon icon='bootstrap' />
+//             <DevIcon icon='github' />
+//             <DevIcon icon='nodejs' />
 
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
-export default Technologies
+// export default Technologies
