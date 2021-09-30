5a2e2a18cb4a51d61d936ab0ab667b679b998e08
janayAl <jsallen298705@gmail.com>
2021-09-30 17:41:56 -0500

    removed quotes from image in portfolioData

--- src/helpers/portfolioData.js
+++ src/helpers/portfolioData.js
@@ -6,7 +6,7 @@ import passWord from "../images/projectsicon/passWordGenerator.png"
 
 const portfolioData = [
     {
-        image: 'fitnessTracker',
+        image: fitnessTracker,
         link: 'https://dry-fortress-22156.herokuapp.com/',
         title: 'Fitness Tracker App',
         summary: 'A fitness tracker app that allows the user to track their exercises and record the type of exercise, amount of weights used, and teh day.',
@@ -14,7 +14,7 @@ const portfolioData = [
 
     },
     {
-        image: 'noteTaker',
+        image: noteTaker,
         link: ' https://jallen-note-taker-express.herokuapp.com/',
         title: 'Note Taker App',
         summary: 'An app that allows the user to take notes and add a title to each note, save the note, and have the note be displayed on the screen while entering a new note.',
@@ -23,7 +23,7 @@ const portfolioData = [
     },
 
     {
-        image: 'passWord',
+        image: passWord,
         link: 'https://janayal.github.io/passwordgenerator/',
         title: 'Password Generator',
         summary: 'A web application that generates a random password based on selections made by the user.',
