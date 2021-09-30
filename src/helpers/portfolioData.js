bc4127873cd70f1efcf7e6ef8017219431c4f152
janayAl <jsallen298705@gmail.com>
2021-09-30 14:16:52 -0500

    import images

---
+++ src/helpers/portfolioData.js
@@ -0,0 +1,26 @@
+import fitnessTracker = "../images/projectsicon/FitnessTrackerApp.png";
+import noteTaker = "../images/projectsicon/NoteTakerApp.png;
+import teamProfile = "../images/projectsicon/TeamProfileGenerator.png";
+import weather = "../images/projectsicon/WeatherApp.jpg";
+import blackShirt = "..images/MeBlackShirt.JPG"
+
+const portfolioData = [
+    {
+        image: 'fitnessTracker',
+        link: '',
+        title: 'Fitness Tracker App',
+        summary: 'A fitness tracker app that allows the user to track their exercises and record the type of exercise, amount of weights used, and teh day.',
+        tech: 'MongoDB, node.js, and JavaScript'
+
+    },
+    {
+        image: '',
+        link: 'https://dry-fortress-22156.herokuapp.com/',
+        title: '',
+        summary: '',
+        tech: ''
+
+    },
+]
+
+export default portfolioData
