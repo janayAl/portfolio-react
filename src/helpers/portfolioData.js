1499fc0d20571609d49958c0292b2b25fbe2a660
janayAl <jsallen298705@gmail.com>
2021-09-30 14:30:14 -0500

    added info for 3 work samples

--- src/helpers/portfolioData.js
+++ src/helpers/portfolioData.js
@@ -1,26 +1,37 @@
 import fitnessTracker = "../images/projectsicon/FitnessTrackerApp.png";
 import noteTaker = "../images/projectsicon/NoteTakerApp.png;
-import teamProfile = "../images/projectsicon/TeamProfileGenerator.png";
 import weather = "../images/projectsicon/WeatherApp.jpg";
-import blackShirt = "..images/MeBlackShirt.JPG"
+import passWord = "../images/projectsicon/passWordGenerator.png"
+
 
 const portfolioData = [
     {
         image: 'fitnessTracker',
-        link: '',
+        link: 'https://dry-fortress-22156.herokuapp.com/',
         title: 'Fitness Tracker App',
         summary: 'A fitness tracker app that allows the user to track their exercises and record the type of exercise, amount of weights used, and teh day.',
         tech: 'MongoDB, node.js, and JavaScript'
 
     },
     {
-        image: '',
-        link: 'https://dry-fortress-22156.herokuapp.com/',
-        title: '',
-        summary: '',
-        tech: ''
+        image: 'noteTaker',
+        link: ' https://jallen-note-taker-express.herokuapp.com/',
+        title: 'Note Taker App',
+        summary: 'An app that allows the user to take notes and add a title to each note, save the note, and have the note be displayed on the screen while entering a new note.',
+        tech: 'JavaScript, Node.js'
 
     },
+
+    {
+        image: 'passWord',
+        link: 'https://janayal.github.io/passwordgenerator/',
+        title: 'Password Generator',
+        summary: 'A web application that generates a random password based on selections made by the user.',
+        tech: 'JavaScript, HTML, CSS'
+
+    },
+
 ]
+
 
 export default portfolioData
