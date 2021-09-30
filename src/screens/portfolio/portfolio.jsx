b9149d517b18d30ee88d8f215763249f0ea5c9c2
janayAl <jsallen298705@gmail.com>
2021-09-30 15:20:30 -0500

    create event listeners for the portfolioData info

--- src/screens/portfolio/portfolio.jsx
+++ src/screens/portfolio/portfolio.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './portfolio.css';
 import portfolioData from '../../helpers/portfolioData';
 import Container from 'react-bootstrap/Container';
@@ -8,14 +8,40 @@ import Image from 'react-bootstrap/Image';
 import Modal from 'react-bootstrap/modal';
 import Button from 'react-bootstrap/Button';
 
-
+//usign bootstrap
+//line 18 and beyond creates an on click function using the info recorded in the model, this is jsx
 function Portfolio() {
+    const [modalShow, setModalShow] = useState(false);
+    const [tempData, setTempData] = useState({})
+    const mapped = portfolioData.map((e, idx) => {
+        return (
+            <Card key={idx}>
+                <Image
+                    onClick={() => {
+                        setTempData({
+                            image: e.image,
+                            link: e.link,
+                            title: e.title,
+                            summary: e.summary,
+                            tech: e.tech
+                        })
+                    }}
+                //closing image tag for image in the jsx way
+                />
+            </Card>
+        )
+    })
     return (
         <div className="portfolio_main_container" id="portfolio">
             <h1>Portfolio</h1>
             <p>Take a peek into my Github Repo!
                 <a href="https://github.com/janayAl" target="blank" rel="noreferrer">https://github.com/janayAl</a>
             </p>
+            <Container fluid="lg" style={{ padding: '2rem 0' }}>
+                <Row>
+
+                </Row>
+            </Container>
         </div>
     )
 }
