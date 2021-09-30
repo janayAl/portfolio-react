c38048b56771a3c17afe049ee099f5073e6d97f6
janayAl <jsallen298705@gmail.com>
2021-09-30 15:07:34 -0500

    added first portion of portfolio section

--- src/screens/portfolio/portfolio.jsx
+++ src/screens/portfolio/portfolio.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './portfolio.css';
+import portfolioData from '../../helpers/portfolioData';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+import Modal from 'react-bootstrap/modal';
+import Button from 'react-bootstrap/Button';
 
-function portfolio() {
+
+function Portfolio() {
     return (
-        <div>
-
+        <div className="portfolio_main_container" id="portfolio">
+            <h1>Portfolio</h1>
+            <p>Take a peek into my Github Repo!
+                <a href="https://github.com/janayAl" target="blank" rel="noreferrer">https://github.com/janayAl</a>
+            </p>
         </div>
     )
 }
 
-export default portfolio
+export default Portfolio
