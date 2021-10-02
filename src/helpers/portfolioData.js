e715a0719c7a95b457a4144d364e8fe524fa8187
janayAl <jsallen298705@gmail.com>
2021-10-02 09:24:41 -0500

    commented out the model section

--- src/helpers/portfolioData.js
+++ src/helpers/portfolioData.js
@@ -31,7 +31,6 @@ const portfolioData = [
 
     },
 
-
 ]
 
 
--- src/screens/portfolio/portfolio.jsx
+++ src/screens/portfolio/portfolio.jsx
@@ -14,38 +14,39 @@ function Portfolio() {
     const [modalShow, setModalShow] = useState(false);
     const [tempData, setTempData] = useState({})
 
-    function createModal(data) {
-        return (
-            <Modal
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-                size="lg"
-                arial-labelledby="container-modal-title-vcenter"
-                centered
-            >
-                <Modal.Header>
-                    <Modal.Title id="container-modal-title-vcenter">
-                        {data.title}
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <p>{data.summary}</p>
-                    <Image src={data.image} style={{ width: '200px' }} />
-                </Modal.Body>
-                <a id="portfolio_modal_link" href={data.link} target="blank" rel="noreferrer">Visit Site</a>
-                <Modal.Footer>
-                    <div>Technologies used: </div>
-                    <p style={{ fontSize: '0.7rem', marginRight: 'auto' }}>{data.tech}</p>
-                    <Button onClick={() => setModalShow(false)}>Close</Button>
-                </Modal.Footer>
-            </Modal >
-        )
+    // function createModal(data) {
+    //     return (
+    //         <Modal
+    //             show={modalShow}
+    //             onHide={() => setModalShow(false)}
+    //             size="lg"
+    //             arial-labelledby="container-modal-title-vcenter"
+    //             centered
+    //         >
+    //             <Modal.Header>
+    //                 <Modal.Title id="container-modal-title-vcenter">
+    //                     {data.title}
+    //                 </Modal.Title>
+    //             </Modal.Header>
+    //             <Modal.Body>
+    //                 <p>{data.summary}</p>
+    //                 <Image src={data.image} style={{ width: '200px' }} />
+    //             </Modal.Body>
+    //             <a id="portfolio_modal_link" href={data.link} target="blank" rel="noreferrer">Visit Site</a>
+    //             <Modal.Footer>
+    //                 <div>Technologies used: </div>
+    //                 <p style={{ fontSize: '0.7rem', marginRight: 'auto' }}>{data.tech}</p>
+    //                 <Button onClick={() => setModalShow(false)}>Close</Button>
+    //             </Modal.Footer>
+    //         </Modal >
+    //     )
 
-    }
+    // }
 
     const mapped = portfolioData.map((e, idx) => {
         return (
             <Card key={idx} id="portfolio_card_container">
+
                 <Image className="portfolio_image"
                     onClick={() => {
                         setTempData({
